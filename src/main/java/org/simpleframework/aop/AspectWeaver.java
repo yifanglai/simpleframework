b9f3348c81408527f14b43a75d89b0624f7ef028
yifanglai <1530683062@qq.com>
2021-03-04 23:55:08 +0800

    aop2.0

--- src/main/java/org/simpleframework/aop/AspectWeaver.java
+++ src/main/java/org/simpleframework/aop/AspectWeaver.java
@@ -97,35 +97,35 @@ public class AspectWeaver {
 	}
 
 
-	private void weaveByCategory(Class<? extends Annotation> category, List<AspectInfo> aspectInfoList) {
-		//1.获取被代理类的集合
-		Set<Class<?>> classSet = beanContainer.getClassesByAnnotation(category);
-		if (ValidationUtil.isEmpty(classSet)){return;}
-		//2.遍历被代理类，分别为每个被代理类生成动态代理实例
-		for (Class<?> targetClass:classSet){
-			//创建动态代理对象
-			AspectListExecutor aspectListExecutor = new AspectListExecutor(targetClass,aspectInfoList);
-			Object proxyBean = ProxyCreator.createProxy(targetClass,aspectListExecutor);
-			//3.将动态代理对象实例添加到容器里，取代未被代理前的类实例
-			beanContainer.addBean(targetClass,proxyBean);
-		}
-		//3.将动态代理对象实例添加到容器里，取代未被代理前的类实例
-	}
+//	private void weaveByCategory(Class<? extends Annotation> category, List<AspectInfo> aspectInfoList) {
+//		//1.获取被代理类的集合
+//		Set<Class<?>> classSet = beanContainer.getClassesByAnnotation(category);
+//		if (ValidationUtil.isEmpty(classSet)){return;}
+//		//2.遍历被代理类，分别为每个被代理类生成动态代理实例
+//		for (Class<?> targetClass:classSet){
+//			//创建动态代理对象
+//			AspectListExecutor aspectListExecutor = new AspectListExecutor(targetClass,aspectInfoList);
+//			Object proxyBean = ProxyCreator.createProxy(targetClass,aspectListExecutor);
+//			//3.将动态代理对象实例添加到容器里，取代未被代理前的类实例
+//			beanContainer.addBean(targetClass,proxyBean);
+//		}
+//		//3.将动态代理对象实例添加到容器里，取代未被代理前的类实例
+//	}
 
-	private void categorizeAspect(Map<Class<? extends Annotation>, List<AspectInfo>> categorizedMap, Class<?> aspectClass) {
-		Order orderTag = aspectClass.getAnnotation(Order.class);
-		Aspect aspectTag = aspectClass.getAnnotation(Aspect.class);
-		DefaultAspect aspect = (DefaultAspect)beanContainer.getBean(aspectClass);
-		AspectInfo aspectInfo = new AspectInfo(orderTag.value(),aspect);
-		if (!categorizedMap.containsKey(aspectTag.value())){
-			List<AspectInfo> aspectInfoList = new ArrayList <>();
-			aspectInfoList.add(aspectInfo);
-			categorizedMap.put(aspectTag.value(),aspectInfoList);
-		}else {
-			List<AspectInfo> aspectInfoList = categorizedMap.get(aspectTag.value());
-			aspectInfoList.add(aspectInfo);
-		}
-	}
+//	private void categorizeAspect(Map<Class<? extends Annotation>, List<AspectInfo>> categorizedMap, Class<?> aspectClass) {
+//		Order orderTag = aspectClass.getAnnotation(Order.class);
+//		Aspect aspectTag = aspectClass.getAnnotation(Aspect.class);
+//		DefaultAspect aspect = (DefaultAspect)beanContainer.getBean(aspectClass);
+//		AspectInfo aspectInfo = new AspectInfo(orderTag.value(),aspect);
+//		if (!categorizedMap.containsKey(aspectTag.value())){
+//			List<AspectInfo> aspectInfoList = new ArrayList <>();
+//			aspectInfoList.add(aspectInfo);
+//			categorizedMap.put(aspectTag.value(),aspectInfoList);
+//		}else {
+//			List<AspectInfo> aspectInfoList = categorizedMap.get(aspectTag.value());
+//			aspectInfoList.add(aspectInfo);
+//		}
+//	}
 
 	/**
 	 * 有@Aspect和@Order标签规范，必须继承自DefaultAspect.class，
@@ -133,10 +133,10 @@ public class AspectWeaver {
 	 * @param aspectClass
 	 * @return
 	 */
-	private boolean verifyAspect(Class<?> aspectClass) {
-		return aspectClass.isAnnotationPresent(Aspect.class)&&
-				aspectClass.isAnnotationPresent(Order.class)&&
-				DefaultAspect.class.isAssignableFrom(aspectClass)&&
-				aspectClass.getAnnotation(Aspect.class).value()!=Aspect.class;
-	}
+//	private boolean verifyAspect(Class<?> aspectClass) {
+//		return aspectClass.isAnnotationPresent(Aspect.class)&&
+//				aspectClass.isAnnotationPresent(Order.class)&&
+//				DefaultAspect.class.isAssignableFrom(aspectClass)&&
+//				aspectClass.getAnnotation(Aspect.class).value()!=Aspect.class;
+//	}
 }
